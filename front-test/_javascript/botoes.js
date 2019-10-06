e58f3f7dc4b2e1d30169759b845f3f7f1d6c92f2
Guilherme Kruszynski <guilherme.kac10@gmail.com>
2019-10-06 17:58:54 -0300

    Adiciona projeto dashboard

---
+++ front-test/_javascript/botoes.js
@@ -0,0 +1,20 @@
+function abrirModal() {
+    document.getElementById('modal').style.top = "0";
+}
+function fecharModal() {
+    document.getElementById('modal').style.top = "-100%";
+}
+
+function abrirModal2() {
+    document.getElementById('modal2').style.top = "0";
+}
+function fecharModal2() {
+    document.getElementById('modal2').style.top = "-100%";
+}
+
+function abrirModal3() {
+    document.getElementById('modal3').style.top = "0";
+}
+function fecharModal3() {
+    document.getElementById('modal3').style.top = "-100%";
+}
